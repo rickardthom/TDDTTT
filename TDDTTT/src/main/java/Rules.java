d82bd7b62c429b60a7cbf60bf073d5d70c1edff0
Rick <Rick@Asus.lan>
2017-11-13 11:19:00 +0100

    REFACTOR

---
+++ TDDTTT/src/main/java/Rules.java
@@ -0,0 +1,23 @@
+
+public class Rules {
+
+	public boolean checkIfBoardFull(Board board){
+		return false;
+	}
+	
+	public boolean checkIfPlayerWon(Board board) {
+		return false;
+	}
+	
+	public boolean checkForColWin() {
+		return false;
+	}
+	
+	public boolean checkForRowWin() {
+		return false;
+	}
+	
+	public boolean checkForDiagWin() {
+		return false;
+	}
+}
