195fec3f6dc740de5dbc2e7d1382c4a5b78e2bd9
Rick <Rick@Asus.lan>
2017-11-14 13:51:47 +0100

    RED

--- TDDTTT/src/test/java/AllTests.java
+++ TDDTTT/src/test/java/AllTests.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
     BoardTest.class,
     RulesTest.class,
     PlayerTest.class,
-    GameManagerTest.class
+    UserInputTest.class
 })
 
 public class AllTests {
---
+++ TDDTTT/src/test/java/GameManagerTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameManagerTest {
+	GameManager game_manager;
+	
+	@Before
+	public void setupTest() {
+		this.game_manager = new GameManager();
+	}
+	
+	@Test
+	public void getIntUserInputAsValidIntTest() {
+	    UserInput user_input = mock(UserInput.class);
+	    
+	    when(user_input.askUserForInt("Give a number between 1 and 3")).thenReturn(1);
+	    
+	    assertEquals(game_manager.getUserRowCol(user_input), 1);
+	}
+	
+	@Test
+	public void getIntUserInputAsInvalidIntTest() {
+	    UserInput user_input = mock(UserInput.class);
+	    
+	    when(user_input.askUserForInt("Give a number between 1 and 3")).thenReturn(4);
+	    when(user_input.askUserForInt("Wrong number, try again.")).thenReturn(3);
+	    
+	    game_manager.getUserRowCol(user_input);
+	    
+	    verify(user_input).askUserForInt("Wrong number, try again.");
+	    
+	}
+}
