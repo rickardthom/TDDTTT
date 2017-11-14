f8ecc051a04a9ed20a974852ca48b88c759c5696
Rick <Rick@Asus.lan>
2017-11-14 09:14:09 +0100

    Covers 100% of board

--- TDDTTT/src/test/java/BoardTest.java
+++ TDDTTT/src/test/java/BoardTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-//Covers 50% of board
+//Covers 100% of board
 public class BoardTest {
 	BoardStub board;
 	
@@ -19,19 +19,77 @@ public class BoardTest {
 	}
 	
 	@Test 
-	public void invalidRangeRowCol() {
-		assertFalse(board.validateRowCol(1,4));
+	public void invalidRangeRowColTest() {
+		assertFalse(board.validateRowCol(-1, 3));
+		assertFalse(board.validateRowCol(3, -1));
+		assertFalse(board.validateRowCol(1, 3));
+		assertFalse(board.validateRowCol(1, -3));
 	}
 	
 	@Test 
-	public void validRangeRowCol() {
+	public void validRangeRowColTest() {
 		assertTrue(board.validateRowCol(2,2));
+		assertTrue(board.validateRowCol(0,0));
+		assertTrue(board.validateRowCol(1,1));
+	}
+	
+	@Test 
+	public void validSymbolOnEmptyBoardTest() {
+		for (int i = 0; i < 3; i++) {
+			for (int x = 0; x < 3; x++) {
+				assertTrue(board.validateRowCol(i, x));
+			}
+		}
+	}
+	
+	@Test 
+	public void validSymbolOnFullBoardTest() {
+		board.setFullBoardWithNoWinner();
+		for (int i = 0; i < 3; i++) {
+			for (int x = 0; x < 3; x++) {
+				assertFalse(board.validateRowCol(i, x));
+			}
+		}
+	}
+	
+	@Test 
+	public void getBoardTest() {
+		board.setSymbolAt(1, 1, 'X');
+		char[][] char_arr = board.getBoard();
+		assertEquals(char_arr[1][1], board.getSymbolAt(1, 1));
+	}
+	
+	@Test 
+	public void boardIsFullTest() {
+		assertFalse(board.isFull());
+		board.setFullBoardWithNoWinner();
+		assertTrue(board.isFull());
+	}
+	
+	// no exception expected
+	@Test(expected = Test.None.class)
+	public void printEmptyAndFullBoardTest() {
+		board.printBoard();
+		board.setFullBoardWithNoWinner();
+		board.printBoard();
 	}
 	
 	public class BoardStub extends Board {
 		
 		public char getSymbolAt(int row, int col) {
 			return board[row][col];
+		}
+		
+		public void setFullBoardWithNoWinner(){
+			setSymbolAt(0, 0, 'X');
+			setSymbolAt(0, 1, 'X');
+			setSymbolAt(0, 2, 'O');
+			setSymbolAt(1, 0, 'O');
+			setSymbolAt(1, 1, 'O');
+			setSymbolAt(1, 2, 'X');
+			setSymbolAt(2, 0, 'X');
+			setSymbolAt(2, 1, 'O');
+			setSymbolAt(2, 2, 'X');
 		}
 	}
 }
