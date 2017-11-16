3da70a7f02a3a3630946dff713aa3acc9842e9ad
Rick <Rick@Asus.lan>
2017-11-16 18:05:13 +0100

    REFACTOR

--- TDDTTT/src/main/java/GameManager.java
+++ TDDTTT/src/main/java/GameManager.java
@@ -10,12 +10,17 @@ public class GameManager {
 	
 	public void start() {
 		showTitle();
-		while(!getUserRowCol());
+		
+		while(true) {
+			board.printBoard();
+			while(!getUserRowCol());
+			board.setSymbolAt(current_player.getRow(), current_player.getCol(), current_player.getSymbol());
+		}
 		
 	}
 	
 	public boolean getUserRowCol() {
-		System.out.println("\n" + current_player.getName() + ", place your marker '" + current_player.getSymbol() + "' in an empty spot!");
+		System.out.println("\n" + current_player.getName() + ", place your marker '" + current_player.getSymbol() + "' in an empty spot! (0-2)");
 		current_player.setRow(user_input.askUserForInt("row: "));
 		current_player.setCol(user_input.askUserForInt("col: "));
 		return board.validateRowCol(current_player.getRow(), current_player.getCol());
